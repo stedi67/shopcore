3df296842ae57a7bdea6d5db69ff0b2d27f6b657
std <stephan.diehl@ableton.com>
2015-05-29 12:44:40 +0200

    convert strings to Decimals and back

--- pm_decimals.nim
+++ pm_decimals.nim
@@ -1,10 +1,14 @@
+#!/usr/bin/env nimdebug
+
+# Poor mans Decimals
+# Do some restricted set of decimal operations with 64 bit ints.
+# The layout is: 60 bit number, 1 bit sign and 3 bit precision
+
 import
   strutils
 
 type
-  Decimal = object
-    value: int
-    prec: Positive
+  Decimal = int64
 
 proc reverse(s: string): string =
   result = ""
@@ -12,18 +16,54 @@ proc reverse(s: string): string =
     result.add s[i]
 
 proc toString(x: Decimal): string =
-  var value_string = intToStr(x.value, x.prec+1).reverse
-  result = newString(value_string.len + 1)
-  for i in 0..result.len - 1:
-    if i < x.prec:
-      result[i] = value_string[i]
-    elif i == x.prec:
-      result[i] = "."[0]
-    elif i > x.prec:
-      result[i] = value_string[i-1]
+  result = ""
+  let prec = 0b111 and x
+  let sign = 0b1000 and x
+  let num = x div 16 
+  var s = $num
+  s = s.reverse
+  var k = prec - s.len + 1
+  while k>0:
+    s.add "0"
+    k = k-1
+
+  for i in 0..s.len:
+    if i < prec:
+      result.add s[i]
+    elif i == prec:
+      result.add "."
+    elif i > prec:
+      result.add s[i-1]
+
   result = result.reverse
+  if sign > 0:
+    result = "-" & result
+
+
+iterator digits(number: string): int =
+  for c in number: 
+    if c in {'0'..'9'}:
+      yield ord(c) - ord('0')
+
+proc toDecimal(description: string): Decimal =
+  result = 0
+  var sign = if description[0] == '-': 0b1000
+                                 else: 0
+
+  let rev = description.reverse
+  let prec = rev.find(".")
+  var multiplier = 1
+  for num in digits(rev):
+    result = result + num * multiplier
+    multiplier = multiplier * 10
+  result = result * 16 # left shift of 3 bits
+  result = result + sign
+  result = result + prec
 
 when isMainModule:
-  assert(toString(Decimal(value:100, prec:2)) == "1.00", "Wrong")
-  assert(toString(Decimal(value:1001, prec:2)) == "10.01", "Wrong")
-  assert(toString(Decimal(value:1, prec:2)) == "0.01", "Wrong")
+  echo "running assetions"
+  assert((toString toDecimal("0.01")) == "0.01")
+  assert((toString toDecimal("1.00")) == "1.00")
+  assert((toString toDecimal("10.24")) == "10.24")
+  assert((toString toDecimal("-1.0")) == "-1.0")
+  assert((toString toDecimal("2.")) == "2.")
