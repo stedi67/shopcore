1726c375a6e86f022ae3d659b18451d6f4856f04
Stephan Diehl <stephan@transvection.de>
2015-06-07 12:36:57 +0200

    removed 'sign' from decimal definition

--- pm_decimals.nim
+++ pm_decimals.nim
@@ -8,20 +8,45 @@ import
   strutils
 
 type
-  Sign = enum
-    positive, negative
   Precision = int8
-  Decimal = tuple[value: int64, prec: Precision, sign: Sign]
+  Decimal = tuple[value: int64, prec: Precision]
 
 proc reverse(s: string): string =
   result = ""
   for i in countdown(high(s), 0):
     result.add s[i]
 
+iterator digits(number: string): int =
+  for c in number:
+    if c in {'0'..'9'}:
+      yield ord(c) - ord('0')
+
+proc newDecimal*(description: string): Decimal =
+  var value = 0.int64
+  let fac = if description[0] == '-': -1
+                                 else: 1
+
+  let rev = description.reverse
+  let prec = rev.find(".").int8
+  var multiplier = 1
+  for num in digits(rev):
+    value = value + num * multiplier
+    multiplier = multiplier * 10
+  let val = (fac*value, prec)
+  result = val
+
+proc `<`*(a, b: Decimal): bool =
+  return a.value < b.value
+
+proc `<`*[T](a: Decimal, b: T): bool =
+  return a.value < b
+
+proc `>=`*[T,S](a: T, b: S): bool =
+  return not (a < b)
+
 proc toString(x: Decimal): string =
   result = ""
-  let
-    (num, prec, sign) = x
+  let (num, prec) = x
   var s = $num
   s = s.reverse
   var k = prec - s.len + 1
@@ -40,85 +65,56 @@ proc toString(x: Decimal): string =
   result = result.reverse
   if result[0] == '.':
     result = "0" & result
-  if sign == negative:
-    result = "-" & result
 
 proc `$`*(x: Decimal): string =
   return x.toString
 
 
-iterator digits(number: string): int =
-  for c in number:
-    if c in {'0'..'9'}:
-      yield ord(c) - ord('0')
-
-proc newDecimal*(description: string): Decimal =
-  var value = 0.int64
-  let sign = if description[0] == '-': negative
-                                 else: positive
-
-  let rev = description.reverse
-  let prec = rev.find(".").int8
-  var multiplier = 1
-  for num in digits(rev):
-    value = value + num * multiplier
-    multiplier = multiplier * 10
-  let val = (value, prec, sign)
-  result = val
-
 proc to_precision*(x: Decimal, prec: Precision): Decimal =
   let factors = [1, 10, 100, 1000, 10_000, 100_000, 1_000_000, 10_000_000]
   let m = x.prec - prec
-  let factor = factors[abs(m)]
-  let new_value = if m > 0: x.value div factor
-                      else: x.value * factor
-  result = (new_value, prec, x.sign)
+  let i = abs(m)
+  var new_value = 0.int64
+  if m > 0:
+    let val = x.value div factors[i-1]
+    if (val - 5) div 10 != val div 10: # we would need to round down
+        new_value = val div 10
+    else: # rounding up
+        let diff = val - (val div 10) * 10
+        new_value = (val + diff) div 10
+  else:
+      new_value = x.value * factors[i]
+  result = (new_value, prec)
 
 proc `+`*(a, b:Decimal): Decimal =
   let prec = max(a.prec, b.prec)
   let new_a = a.to_precision(prec)
   let new_b = b.to_precision(prec)
-  let a_v:int64 = if a.sign == negative: - new_a.value
-                             else: new_a.value
-  let b_v:int64 = if b.sign == negative: - new_b.value
-                             else: new_b.value
-  let c_v = a_v.int64 + b_v.int64
-  let c_sign = if c_v < 0: negative
-                     else: positive
-  result = (c_v, prec, c_sign)
+  result = (new_a.value + new_b.value, prec)
 
 proc `-`*(a:Decimal): Decimal =
-  let sign = if a.sign == positive: negative
-                                 else: positive
-  result = (a.value, a.prec, sign)
+  result = (-a.value, a.prec)
 
 proc `-`*(a, b:Decimal): Decimal =
   result = a + (-b)
 
 proc `*`*[T](a: T, b:Decimal): Decimal =
-  if a == 0:
-    result = newDecimal("0")
-  else:
-    result = b
-    var i = abs(a) - 1
-    while i > 0:
-      result = result + b
-      i -= 1
-    if a < 0:
-      result.value = -1 * result.value
+  result = (a*b.value, b.prec)
 
 proc `==`*(a, b: Decimal): bool =
-  result = a.value == b.value and a.prec == b.prec and a.sign == b.sign
+  result = a.value == b.value and a.prec == b.prec
 
 when isMainModule:
   echo "running assetions"
   assert($newDecimal("0.01") == "0.01")
   assert($newDecimal("1.00") == "1.00")
   assert($newDecimal("10.24") == "10.24")
+  let val = newDecimal("-1.0")
+  echo ($val)
   assert($newDecimal("-1.0") == "-1.0")
   assert($newDecimal("2.") == "2.")
-  let a = to_precision((100.int64, 0.Precision, positive), 1)
-  let b = (1000.int64, 1.Precision, positive)
+  let a = to_precision((100.int64, 0.Precision), 1)
+  let b = (1000.int64, 1.Precision)
   assert($a == $b)
   assert($a == "100.0")
   assert((newDecimal("0.12") + newDecimal("1.32")) == newDecimal("1.44"))
