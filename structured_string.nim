3758351291a38527a4b41267df0b362e3edb2867
std <stephan.diehl@ableton.com>
2015-07-23 12:22:58 +0200

    structured string with nice syntax

--- structured_string.nim
+++ structured_string.nim
@@ -12,44 +12,29 @@ proc new$2(text:string):$1 =
   result = $1(text)
 """
 
-macro StructuredString(name: string{lit}, definition: string{lit}): stmt {.immediate.} =
+macro StructuredString(name: string, deflist: stmt): stmt {.immediate.} =
   var
-    low = 0
-    high= 0
-  echo definition
-  result = newStmtList()
-  result.add(parseStmt("type $1 = string" % name.strVal))
-  for item in split(definition.strVal, ','):
-    let newItem = replace(item, " ")
-    let key_value = split(newItem, ':')
-    let fieldName = key_value[0]
-    let fieldLen = parseInt(key_value[1])
-    high = high + fieldLen
-    result.add(parseStmt("proc $#(base:$#): string = result = base[$#..$#]" % [fieldName, name.strVal, $low, $(high-1)]))
-    low = low + fieldLen
-  result.add(parseStmt(newFuncText % [name.strVal, name.strVal.capitalize, $high]))
+    low:BiggestInt = 0
+    high:BiggestInt = 0
+    fieldName: string
+    fieldLen: BiggestInt
 
-let acdef = """
-product_id:4,
-version:2,
-channel:2,
-right:2,
-required_product:4,
-rop:2,
-delivery:2,
-num_instances:1,
-serial_number_type:1,
-language:2,
-customer_group_discount:2,
-license_dependent_discount:2
-"""
-StructuredString("SoftwareArticleCode", "a:2")
+  result = newStmtList()
+  result.add(parseStmt("type $1 = string" % $name))
+  for i in 0..deflist.len-1:
+    fieldName = $deflist[i][0].ident
+    fieldLen = deflist[i][1][0].intVal
+    high = high + fieldLen
+    result.add(parseStmt("proc $#(base:$#): string = result = base[$#..$#]" % [fieldName, $name, $low, $(high-1)]))
+    low = low + fieldLen
+  result.add(parseStmt(newFuncText % [$name, ($name).capitalize, $high]))
 
 when isMainModule:
-  StructuredString("test", 10, "a:5, b:5")
+  StructuredString(test):
+    a:5
+    b:5
 
   var foo = newTest("aaaaabbbbb")
-
   echo "running assetions"
   assert foo.a == "aaaaa"
   assert foo.b == "bbbbb"
